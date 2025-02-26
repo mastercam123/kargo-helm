308b97d7a2c4f34421b8567e4dd993d895d6c4ee
mastercam123 <k4mbingoce@yahoo.com>
2025-02-26 15:01:10 +0100

    Added new flask project.

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    image_version = os.getenv("IMAGE_VERSION", "unknown")
+    return "Hello from Kargo! 🚀 Version: " + image_version
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
